2eec24275057e92b4dec39a28925fed46bad256a
Dao Ngoc Hoa - CNTT2 <124851971+DaoNgocHoa@users.noreply.github.com>
2023-02-27 23:21:39 +0700

    Update Point2D.java

--- 26-2-2023 bai lab so 3/BT-1-classroom/bai1/Point2D.java
+++ 26-2-2023 bai lab so 3/BT-1-classroom/bai1/Point2D.java
@@ -4,18 +4,22 @@ public class Point2D
 {
     private float x=0f;
     private float y=0f;
-    Point2D(){
+    Point2D()
+    {
         this.x=0;
         this.y=0;
     };
-    Point2D(float x,float y){
+    Point2D(float x,float y)
+    {
         this.x=x;
         this.y=y;
     };
-    float getX(){
+    float getX()
+    {
         return x;
     };
-    float getY(){
+    float getY()
+    {
         return y;
     };
 }
