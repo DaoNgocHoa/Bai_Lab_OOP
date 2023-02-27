0e373c3fd6bca1e07dc86ebd53c663a4ff053796
Dao Ngoc Hoa - CNTT2 <124851971+DaoNgocHoa@users.noreply.github.com>
2023-02-27 23:22:57 +0700

    Update Fraction.java

--- 26-2-2023 bai lab so 3/BT-1-classroom/bai1/Fraction.java
+++ 26-2-2023 bai lab so 3/BT-1-classroom/bai1/Fraction.java
@@ -4,72 +4,90 @@ public class Fraction
 {
     private int numerator;
     private int denominator;
-    public Fraction(){
+    public Fraction()
+    {
         numerator=0;
         denominator=1;
     };
 
-    public int getNumerator() {
+    public int getNumerator() 
+    {
         return numerator;
     }
 
-    public void setNumerator(int numerator) {
+    public void setNumerator(int numerator) 
+    {
         this.numerator = numerator;
     }
 
-    public int getDenominator() {
+    public int getDenominator() 
+    {
         return denominator;
     }
 
-    public void setDenominator(int denominator) {
+    public void setDenominator(int denominator) 
+    {
         this.denominator = denominator;
     }
 
-    public Fraction(int num,int den){
+    public Fraction(int num,int den)
+    {
         if(den==0){
             System.out.println("mau so phai khac 0!!");
             num=0;
             den=10;
-        } else{
+        } 
+        else
+        {
             this.numerator=num;
             this.denominator=den;
         }
     };
-    public Fraction( Fraction f ){
+    public Fraction( Fraction f )
+    {
         this.numerator=f.numerator;
         this.denominator=f.denominator;
     };
-    public Fraction add(Fraction f){
+    public Fraction add(Fraction f)
+    {
         int tuso=(numerator*f.denominator)+(denominator*f.numerator);
         int mauso=denominator*f.denominator;
         return new Fraction(tuso,mauso);
 
     };
-    public Fraction sub(Fraction f){
+    public Fraction sub(Fraction f)
+    {
         int tuso=(numerator*f.denominator)-(denominator*f.numerator);
         int mauso=denominator*f.denominator;
         return new Fraction(tuso,mauso);
 
     };
-    public Fraction mul(Fraction f){
+    public Fraction mul(Fraction f)
+    {
         int tuso=numerator*f.numerator;
         int mauso=denominator*f.denominator;
         return new Fraction(tuso,mauso);
 
     };
-    public Fraction div(Fraction f){
+    public Fraction div(Fraction f)
+    {
         int tuso=numerator*f.denominator;
         int mauso=denominator*f.numerator;
         return new Fraction(tuso,mauso);
 
     };
-    public Fraction reduce(){
+    public Fraction reduce()
+    {
         int a=numerator;
         int b=denominator;
-        while(numerator!=denominator){
-            if(numerator>denominator){
+        while(numerator!=denominator)
+        {
+            if(numerator>denominator)
+            {
                 numerator-=denominator;
-            } else {
+            }
+            else 
+            {
                 denominator-=numerator;
             }
         }
@@ -79,7 +97,8 @@ public class Fraction
 
     };
     @Override
-    public String toString(){
+    public String toString()
+    {
         return numerator+"/"+denominator;
     };
 }
