268375e5fe16f8ce40199b85abbc0c1d56a1ed8f
milkyway <hoad3579@gmail.com>
2023-03-05 00:36:18 +0700

    bai lab so 4 ngay 5/3/2023

---
+++ ke_thua/src/chuyenxe/Main.java
@@ -0,0 +1,18 @@
+package chuyenxe;
+
+class vehicle
+{
+    public void bicyclecolor;
+    {
+        System.out.println("red");
+    }
+    public void carcolor;
+    {
+        System.out.println("black");
+    }
+    
+}
+class bicycle extends vehicle
+{
+    
+}
